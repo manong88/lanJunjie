c623ced998d920e41795f587b18eb42108e848a1
暴躁熊 <1518815428@qq.com>
2022-11-04 16:38:31 +0800

    初始化

---
+++ src/component/catalogue.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+const Catalogue = () => {
+    const [catalogue,setCatalogue] = useState(
+        [
+            {
+                id:1,
+                name:"主页"
+            },
+            {
+                id:2,
+                name:"介绍"
+            }
+        ]
+    )
+
+
+    return (
+        <div>
+            <ul>
+                {
+                    catalogue.map(item=>{
+                        return (
+                            <li key={item.id}>{item.name}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default Catalogue;
