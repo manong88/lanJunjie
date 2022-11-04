3a2285f2c38beb5348243081ce4f852a99718aab
暴躁熊 <1518815428@qq.com>
2022-11-04 17:13:51 +0800

    修改BUG

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  publicDir: 'public', // 静态资源服务的文件夹
+  cacheDir: 'node_modules/.vite', // 存储缓存文件的目录
 })
